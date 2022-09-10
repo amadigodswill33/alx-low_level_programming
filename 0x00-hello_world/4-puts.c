ab5268494448e700c9605525ee920c32bf702d7d
Godswill Amadi <amadigodswill33@gmail.com>
2022-09-10 10:09:07 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the string using the put funtion
+ * 
+ * Description: using the main function
+ * this program prints "programming is like building a multilingual puzzle
+ * Returns: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
