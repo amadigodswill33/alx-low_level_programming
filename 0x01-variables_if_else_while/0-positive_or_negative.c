fe946fd4fca6d432672f043d4319dd760346e760
Godswill Amadi <amadigodswill33@gmail.com>
2022-09-12 14:08:58 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-	printf("%i is positve\n", n);
+	printf("%i is positive\n", n);
 }
 else if (n == 0)
 {
-	pringf("%i is zero\n", n);
+	printf("%i is zero\n", n);
 }
 else if (n < 0)
 {
